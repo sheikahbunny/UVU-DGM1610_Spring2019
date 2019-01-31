df1df0b1de14b0c287d77d672c711bc8ad376710
Julianna Savage <bunnyhoodedsheikah@gmail.com>
2019-01-30 17:06:21 -0700

    Function Scripting

---
+++ 2D Game/Assets/Scripts/Assignments/Functions.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Functions : MonoBehaviour
+{
+
+    public string weather;
+
+    void Start()
+    {
+
+        Weather(weather);
+
+    }
+    void Weather(string weatherState)
+    {
+        if (weatherState == "Sunny")
+        {
+            print("Eew, the suns gross!");
+        }
+        else if (weatherState == "Raining")
+        {
+            print("YES, its FINALLY raining! Where's my umbrella");
+        }
+        else if (weatherState == "Windy")
+        {
+            print("gotta grab my jacket, it's cold.");
+
+        }
+        else if (weatherState == "Snowing")
+        {
+            print("brrr... it's time to grab my snowboard!");
+
+        }
+        else
+        {
+            print("Looks like theres nothing here " + weatherState);
+
+
+        }
+
+
+    }
+
+
+
+
+}
