78e81122bee814eb1ff5615995f546dda7dc0166
Julianna Savage <bunnyhoodedsheikah@gmail.com>
2019-02-10 19:25:30 -0700

    Added player jump

---
+++ 2D Game/Assets/Scripts/TestJump.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestJump : MonoBehaviour
+{
+
+    public bool onGround;
+    private Rigidbody2D rb;
+    public float jumpSpeed;
+
+    void Start()
+    {
+        onGround = true;
+        rb = gameObject.GetComponent<Rigidbody2D>();
+
+    }
+
+
+    void Update()
+    {
+        if (onGround)
+        {
+
+
+            if (Input.GetKey(KeyCode.W))
+            {
+
+                rb.velocity = new Vector2(gameObject.GetComponent<Rigidbody2D>().velocity.x,jumpSpeed);
+                onGround = true;
+            }
+        }
+    }
+}
