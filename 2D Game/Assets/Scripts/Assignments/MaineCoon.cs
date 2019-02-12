457a659090fd9e475760242b0488f04d8a8cb00d
Julianna Savage <bunnyhoodedsheikah@gmail.com>
2019-02-11 17:18:00 -0700

    Added Maine Coon to the field
    Got familiar with inheritance

---
+++ 2D Game/Assets/Scripts/Assignments/MaineCoon.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaineCoon : Feline {
+
+
+    public bool banded;
+
+
+
+	// Use this for initialization
+	void Start () {
+        print("MEOW!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
