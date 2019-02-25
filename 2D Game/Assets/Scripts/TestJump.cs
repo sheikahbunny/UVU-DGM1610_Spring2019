7f8add4232b0d736758068ebb1dcc04b06cd51bf
Julianna Savage <bunnyhoodedsheikah@gmail.com>
2019-02-25 15:36:47 -0700

    got everything set up for coinpick up 2D game

--- 2D Game/Assets/Scripts/TestJump.cs
+++ 2D Game/Assets/Scripts/TestJump.cs
@@ -4,31 +4,33 @@ using UnityEngine;
 
 public class TestJump : MonoBehaviour
 {
-
+    //Player movement variables
     public bool onGround;
     private Rigidbody2D rb;
     public float jumpSpeed;
-    
+    //Player grounded variables
+    private bool grounded = true;
 
     void Start()
     {
-        onGround = true;
+        grounded = true;
         rb = gameObject.GetComponent<Rigidbody2D>();
 
     }
 
 
     void Update()
+    //Make player jump
     {
-        if (onGround)
+        
         {
 
 
-            if (Input.GetKey(KeyCode.W))
+            if (Input.GetKeyDown(KeyCode.W) && grounded)
             {
 
                 rb.velocity = new Vector2(gameObject.GetComponent<Rigidbody2D>().velocity.x,jumpSpeed);
-                onGround = true;
+                grounded = true;
             }
         }
     }
