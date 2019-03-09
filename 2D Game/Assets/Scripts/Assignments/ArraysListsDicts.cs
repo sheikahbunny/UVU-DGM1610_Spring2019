4538526b7add6186442b384dbf9ee86bc505f0d3
Julianna Savage <bunnyhoodedsheikah@gmail.com>
2019-03-08 19:39:46 -0700

    Arrays,Lists,Dicts
    Wednesdays assignment online

---
+++ 2D Game/Assets/Scripts/Assignments/ArraysListsDicts.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArraysListsDicts : MonoBehaviour {
+
+    public string client1 = "Greg";
+
+    public string client2 = "Kate";
+
+    public string client3 = "Adam";
+
+    public string client4 = "Mia";
+
+    public string client5 = "Fred";
+
+    //Array Example
+    public string[] clientList = new string[] { "Greg", "Kate", "Adam", "Mia", "Fred" };
+    //List Example
+    public List<string> santasList = new List<string>();
+
+    public ArrayList inventory = new ArrayList();
+    // Use this for initialization
+    void Start () {
+        santasList.Add("Jimmy");
+        santasList.Add("Jenny");
+        santasList.Add("Sam");
+        santasList.Add("Ty");
+        santasList.Add("Susan");
+        santasList.Remove("Jimmy");
+
+        print(clientList[2]);
+        print(santasList[3]);
+
+        inventory.Add(10);
+        inventory.Add("Bob");
+        inventory.Add(true);
+        inventory.Add(5.2341);
+
+        print(inventory[2]);
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
