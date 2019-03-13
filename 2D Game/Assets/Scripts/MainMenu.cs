30a9407a77614d5ccf8558288de9d0ba0a5dbc64
Julianna Savage <bunnyhoodedsheikah@gmail.com>
2019-03-13 17:15:20 -0600

    Main Menu
    Created a main menu for 2D game

---
+++ 2D Game/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+    //Indexed Level to load
+    public int levelToLoad;
+    //This function loads the level stored in the levelToLoad variable
+    public void LoadLevel()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+    //This function allows you to exit the level/game
+    public void LevelExit()
+    {
+        Application.Quit();
+    }
+    
+	
+	
+}
