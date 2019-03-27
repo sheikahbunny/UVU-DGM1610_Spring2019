d8f98f045755973935ea173f1f1af0bca18f4c63
Julianna Savage <bunnyhoodedsheikah@gmail.com>
2019-03-27 17:01:24 -0600

    LevelManager and Deathbox

---
+++ 2D Game/Assets/Scripts/Assignments/LevelManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+    public GameObject currentCheckPoint;
+    private Rigidbody2D pcRigid;
+
+    private GameObject player;
+    // Particles
+    public GameObject deathParticle;
+    public GameObject respawnParticle;
+    // Respawn Delay
+    public float respawnDelay;
+    //Point Penalty on Death
+    public int pointPenaltyOnDeath;
+    //Store Gravity Value
+    private float gravityStore;
+
+     void Start ()
+    {
+        pcRigid = GameObject.Find("Player").GetComponent<Rigidbody2D>();
+        player = GameObject.Find("Player");
+    }
+}
---
+++ 2D Game/Assets/Scripts/Assignments/Loops.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loops : MonoBehaviour {
+    public int bottles = 0;
+    int count = 0;
+
+    void Start() {
+        //for Loops
+        for (int i = 100; i > bottles; --i)
+        {
+            print(i + "bottles of beer on the wall");
+        }
+
+        //Do While Loops
+        bool shouldContinue = false;
+
+        do
+        {
+            print("Hello World");
+        } while (shouldContinue == true);
+
+        //For Each Loop
+        string[] strings = new string[3];
+
+        strings[0] = "First string";
+        strings[1] = "Second string";
+        strings[2] = "Third string";
+
+        foreach(string item in strings)
+        {
+            print (item);
+        }
+
+
+    }
+    
+}
+
+ 
