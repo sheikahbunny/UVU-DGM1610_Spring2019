47a1bdd2e5828bb9c5e947921fdb81ae9b734353
Julianna Savage <bunnyhoodedsheikah@gmail.com>
2019-04-03 17:15:37 -0600

    Game Update 4/3 completed the necessary homework additions to dictionary scripts

---
+++ 2D Game/Assets/Scripts/Assignments/CameraFollow.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public PlayerMovement player;
+
+    public bool isFollowing;
+
+    
+
+
+    //camera position offset
+
+
+    public float xOffset;
+    public float yOffset;
+
+	// Use this for initialization
+	void Start () {
+        player = FindObjectOfType<PlayerMovement>();
+
+        isFollowing = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (isFollowing)
+        {
+            transform.position = new Vector3(player.transform.position.x + xOffset, player.transform.position.y + yOffset, player.transform.position.z);
+        }
+	}
+}
---
+++ 2D Game/Assets/Scripts/Assignments/Dictionaries.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dictionaries : MonoBehaviour {
+
+    public Hashtable personalDetails = new Hashtable();
+    public Hashtable fruitDetails = new Hashtable();
+    public Hashtable farmAnimals = new Hashtable();
+	void Start () {
+        personalDetails.Add("firstName", "Greg");
+        personalDetails.Add("lastName", "Lukosek");
+        personalDetails.Add("gender", "male");
+        personalDetails.Add("isMarried", true);
+        personalDetails.Add("age", 29);
+
+
+        fruitDetails.Add("fruitName", "Apple");
+        fruitDetails.Add("color", "Red");
+        fruitDetails.Add("treeFruit", true);
+        fruitDetails.Add("seeds", true);
+        fruitDetails.Add("crunchy", true);
+
+
+        fruitDetails.Add("fruitName", "Banana");
+        fruitDetails.Add("color", "Yellow");
+        fruitDetails.Add("treeFruit", true);
+        fruitDetails.Add("seeds", true);
+        fruitDetails.Add("crunchy", false);
+
+
+        fruitDetails.Add("fruitName", "Grape");
+        fruitDetails.Add("color", "Purple");
+        fruitDetails.Add("treeFruit", false);
+        fruitDetails.Add("seeds", true);
+        fruitDetails.Add("crunchy", true);
+
+
+        farmAnimals.Add("animalName", "Cow");
+        farmAnimals.Add("produceMilk", true);
+        farmAnimals.Add("age", 7);
+        farmAnimals.Add("gender", "female");
+
+
+        farmAnimals.Add("animalName", "Chicken");
+        farmAnimals.Add("produceMilk", false);
+        farmAnimals.Add("age", 3);
+        farmAnimals.Add("gender", "male");
+
+
+        farmAnimals.Add("animalName", "Pig");
+        farmAnimals.Add("produceMilk", true);
+        farmAnimals.Add("age", 10);
+        farmAnimals.Add("gender", "female");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
