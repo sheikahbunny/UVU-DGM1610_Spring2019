7ce90f0bdf734b27c479a1e4154b10d5b5d4e387
Julianna Savage <bunnyhoodedsheikah@gmail.com>
2019-04-10 15:36:43 -0600

    Finished Enemy Patrol Script

--- 2D Game/Assets/Scripts/EnemyPatrol.cs
+++ 2D Game/Assets/Scripts/EnemyPatrol.cs
@@ -20,6 +20,16 @@ public class EnemyPatrol : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         notAtEdge = Physics2D.OverlapCircle(edgeCheck.position, wallCheckRadius, whatIsWall);
-        hittingWall
+        hittingWall = Physics2D.OverlapCircle(wallCheck.position, wallCheckRadius, whatIsWall);
+
+        if(hittingWall || !notAtEdge)
+        {
+            moveRight = !moveRight;
+        }
+        if (moveRight)
+        {
+            transform.localScale = new Vector3(-11f, 11f, 1f);
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        }
 	}
 }
