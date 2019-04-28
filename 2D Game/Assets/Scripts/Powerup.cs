0bfb0af3cf8c8c8702d0ce6733cfa664df80f7bc
Julianna Savage <bunnyhoodedsheikah@gmail.com>
2019-04-27 20:31:48 -0600

    Powerup, Particle System
    I put in my powerup prefab and created 3 particle systems.

---
+++ 2D Game/Assets/Scripts/Powerup.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Powerup : MonoBehaviour {
+    public float multiplier = 1.7f;
+    public float duration;
+    public GameObject pickupEffect;
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            StartCoroutine( Pickup(other) );
+        }
+    }
+
+    IEnumerator Pickup(Collider2D player)
+    {
+        Instantiate(pickupEffect, transform.position, transform.rotation);
+        PlayerMovement speedup = player.GetComponent<PlayerMovement>();
+        speedup.moveSpeed *= multiplier;
+
+        GetComponent<Collider2D>().enabled = false;
+        GetComponent<SpriteRenderer>().enabled = false;
+
+        yield return new WaitForSeconds(duration);
+
+        speedup.moveSpeed /= multiplier;
+
+
+        Destroy(gameObject);
+    }
+}
